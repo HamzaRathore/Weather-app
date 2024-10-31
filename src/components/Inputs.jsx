9bc94ee5c1c0da9df8d7434cc5d87c0b2185c926
HamzaRathore <128692254+HamzaRathore@users.noreply.github.com>
2024-10-31 21:05:20 +0500

    Inputs.jsx

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 // @ts-ignore
 import { UilSearch } from '@iconscout/react-unicons'
 import { UilLocationPoint } from '@iconscout/react-unicons'
-function Inputs() {
+function Inputs(setQuery,units,setUnits) {
+
+    const[city,setCity]=useState("");
+
+    const handleSearch=()=>{
+        if(city !=="")
+            setQuery({q:city});
+    }
   return (
     <div className='flex justify-center my-6'>
       <div  className='flex w-3/4 items-center justify-center space-x-4'>
-        <input placeholder='Search for city...' type='text' className='text-xl font-light p-2 w-full shadow-xl outline-none capitalize placeholder:lowercase'></input>
-        <UilSearch size={25} className="text-white cursor-pointer transition ease-out hover:scale-125 "/>
+        <input
+        value={city}
+        onChange={(e)=>setCity(e.currentTarget.value)}
+        placeholder='Search for city...' type='text' className='text-xl font-light p-2 w-full shadow-xl outline-none capitalize placeholder:lowercase'></input>
+        <UilSearch size={25}
+        onClick={handleSearch}
+        className="text-white cursor-pointer transition ease-out hover:scale-125 "/>
         <UilLocationPoint  size={25} className="text-white cursor-pointer transition ease-out hover:scale-125"/>
       </div>
       <div className='flex flex-row w-1/4 items-center justify-center'>
